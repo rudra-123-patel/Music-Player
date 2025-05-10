5586e77f737e0a78066f29ed6465cc357dbf6462
rudra-123-patel <rudrapatel1054@gmail.com>
2025-05-10 17:11:04 +0530

    edited js file

--- script.js
+++ script.js
@@ -21,4 +21,81 @@ var arr = [
   },
 ];
 
-arr.forEach()
+var allSongs = document.querySelector("#all-songs");
+var poster = document.querySelector("#left");
+var play = document.querySelector("#play");
+var forward = document.querySelector("#forward");
+var backward = document.querySelector("#backward");
+
+var audio = new Audio();
+
+var selectedSong = 0;
+
+function mainFunction() {
+  var clutter = "";
+
+  arr.forEach(function (elem, index) {
+    clutter += `
+                <div class="song-card" id=${index}>
+                    <div class="part1">
+                        <img src=${elem.img} alt="" srcset="">
+                        <h2>${elem.songName}</h2>
+                    </div>
+                    <h6>3:56</h6>
+                </div>`;
+  });
+
+  allSongs.innerHTML = clutter;
+
+  audio.src = arr[selectedSong].url;
+  poster.style.backgroundImage = `url(${arr[selectedSong].img})`;
+}
+
+mainFunction();
+
+allSongs.addEventListener("click", function (dets) {
+  selectedSong = dets.target.id;
+
+  mainFunction();
+  play.innerHTML = `<i class="ri-pause-line"></i>`;
+  flag = 1;
+  audio.play();
+});
+
+var flag = 0;
+
+play.addEventListener("click", function () {
+  if (flag == 0) {
+    play.innerHTML = `<i class="ri-pause-line"></i>`;
+    mainFunction();
+    audio.play();
+    flag = 1;
+  } else {
+    play.innerHTML = `<i class="ri-play-fill"></i>`;
+    mainFunction();
+    audio.pause();
+    flag = 0;
+  }
+});
+
+forward.addEventListener("click", function (dets) {
+  if (selectedSong < arr.length - 1) {
+    selectedSong++;
+    mainFunction();
+    audio.play();
+    play.innerHTML = `<i class="ri-pause-line"></i>`;
+  } else {
+    forward.style.opacity = 0.4;
+  }
+});
+
+backward.addEventListener("click", function (dets) {
+  if (selectedSong > 0) {
+    selectedSong--;
+    mainFunction();
+    audio.play();
+    play.innerHTML = `<i class="ri-pause-line"></i>`;
+  } else {
+    backward.style.opacity = 0.4;
+  }
+});
