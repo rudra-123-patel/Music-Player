f819bcbbe0b5cc90d55315fefe13181b88961815
rudra-123-patel <rudrapatel1054@gmail.com>
2025-05-10 15:42:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var arr = [
+  {
+    songName: "Jale 2",
+    url: "./songs/youtubemusic_songs_Jale 2.mp3",
+    img: "./images/jale.jpg",
+  },
+  {
+    songName: "Pehle Bhi Main",
+    url: "./songs/youtubemusic_songs_Pehle Bhi Main.mp3",
+    img: "./images/animal.jpg",
+  },
+  {
+    songName: "Ram Siya Ram",
+    url: "./songs/youtubemusic_songs_Ram Siya Ram.mp3",
+    img: "./images/ram.jpg",
+  },
+  {
+    songName: "Arjan Valley",
+    url: "./songs/youtubemusic_songs_Arjan Vailly Ne.mp3",
+    img: "./images/animal.jpg",
+  },
+];
+
+arr.forEach()
